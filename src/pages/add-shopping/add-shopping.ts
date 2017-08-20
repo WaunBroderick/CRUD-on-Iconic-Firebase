4b6586d300e458d491df607c3a1cd84a9d4ac1fb
Waun Broderick <broderick.wi@live.ca>
2017-08-19 20:35:42 -0400

    Establish NgMOdels for the related varibales being tracked

--- src/pages/add-shopping/add-shopping.ts
+++ src/pages/add-shopping/add-shopping.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { ShoppingItem } from '../../models/shopping-item/shopping-item.interface';
 
 @Component({
   selector: 'page-add-shopping',
@@ -8,11 +9,16 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class AddShoppingPage {
 
+  //Creating a new Object
+  shoppingItem = {} as ShoppingItem
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddShoppingPage');
-  }
+ addShoppingItem(shoppingItem: ShoppingItem){
+   
+  // Log Results to console
+  console.log(shoppingItem);
+ }
 
 }
